8ebfccc3f7f56759ec55523301204f3426058001
Maximiliano Mussuto <mossmax62@gmail.com>
2017-01-19 12:28:47 -0300

    logo

--- app/hero-search.service.ts
+++ app/hero-search.service.ts
@@ -8,11 +8,7 @@ export class HeroSearchService {
   constructor(private http: Http) {}
 
   search(term: string): Observable<Hero[]> {
-<<<<<<< HEAD
   	console.log("HeroSearchService");
-=======
-  	console.log("search");
->>>>>>> 90bbe787d5f9bae6e283f7ac2dd932f3658bf954
     return this.http
                .get(`app/heroes/?name=${term}`)
                .map((r: Response) => r.json().data as Hero[]);
